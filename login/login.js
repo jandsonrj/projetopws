3566e5dc4f7239a825a5715c0749f0aa202c6879
JANDSON VITORINO <101944851+jandsonrj@users.noreply.github.com>
2023-08-21 19:27:28 -0300

    adjustes and updates

--- login/login.js
+++ login/login.js
@@ -1,54 +1,48 @@
-function entrar(){
-    let email = document.querySelector('#email')
-    let emailLabel = document.querySelector('#emailLabel')
+function entrar() {
+    let email = document.querySelector('#email');
+    let emailLabel = document.querySelector('#emailLabel');
 
-    let senha = document.querySelector('#senha')
-    let senhaLabel = document.querySelector('#senhaLabel')
+    let senha = document.querySelector('#senha');
+    let senhaLabel = document.querySelector('#senhaLabel');
 
-    let msgError = document.querySelector('#msgError')
-    let listaUser = []
+    let msgError = document.querySelector('#msgError');
+    let listaUser = [];
 
     let userValid = {
         nome: '',
         email: '',
         senha: ''
-    }
+    };
 
-    listaUser = JSON.parse(localStorage.getItem('listaUser'))
+    listaUser = JSON.parse(localStorage.getItem('listaUser')) || [];
 
-    listaUser.forEach((item) => {
-        if(email.value == item.emailCad && senha.value == item.senhaCad){
-            
-            userValid = { 
-                nome: item.nomeCad,
-                email: item.emailCad,
-                senha: item.senhaCad
-            }
-        }
-    })
-    //console.log(userValid)
-    //console.log(email.value, senha.value)
-    
-    if(email.value == userValid.email && senha.value == userValid.senha){
-        if(email.value == '' ) return
+    let foundUser = listaUser.find(item => email.value === item.emailCad && senha.value === item.senhaCad);
+
+    if (foundUser) {
+        userValid = {
+            nome: foundUser.nomeCad,
+            email: foundUser.emailCad,
+            senha: foundUser.senhaCad
+        };
+
+        if (email.value === '') return;
+
         setTimeout(function() {
             window.location.href = './../dashboard/index.html';
         }, 3000);
-        
-       
-        let mathRandom = Math.random().toString(16).substr(2)
-        let token = mathRandom + mathRandom    
-        
-        localStorage.setItem('token', token)
-        localStorage.setItem('userLogado', JSON.stringify(userValid))
+
+        let mathRandom = Math.random().toString(16).substr(2);
+        let token = mathRandom + mathRandom;
+
+        localStorage.setItem('token', token);
+        localStorage.setItem('userLogado', JSON.stringify(userValid));
     } else {
-        emailLabel.setAttribute('style', 'color: red')
-        email.setAttribute('style', 'border-color: red')
-        senhaLabel.setAttribute('style', 'color: red')
-        senha.setAttribute('style', 'border-color: red')
-        msgError.setAttribute('style', 'display: block')
-        msgError.innerHTML = 'Email ou senha incorretos'
-        email.focus()
+        emailLabel.setAttribute('style', 'color: red');
+        email.setAttribute('style', 'border-color: red');
+        senhaLabel.setAttribute('style', 'color: red');
+        senha.setAttribute('style', 'border-color: red');
+        msgError.setAttribute('style', 'display: block');
+        msgError.textContent = 'Email ou senha incorretos';
+        email.focus();
     }
 }
-
